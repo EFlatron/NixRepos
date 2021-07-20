6a350334b9a4b0bb41e01062ab60768d62eb8e28
EFlatron <small.fin.murlock@gmail.com>
2021-07-20 15:20:00 +0300

    Create util for properties

--- unit11reflection/src/main/java/properties/PropertiesUtil.java
+++ unit11reflection/src/main/java/properties/PropertiesUtil.java
@@ -1,4 +1,17 @@
 package properties;
 
+import java.io.*;
+import java.util.Properties;
+
 public class PropertiesUtil {
+    public static Properties readProperties(String path) {
+        File propsFile = new File(path);
+        Properties properties = new Properties();
+        try(Reader in = new FileReader(propsFile)){
+            properties.load(in);
+        } catch (IOException e){
+            throw new UncheckedIOException(e);
+        }
+        return properties;
+    }
 }
