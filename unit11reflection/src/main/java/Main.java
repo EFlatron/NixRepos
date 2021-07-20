2768213b94d253c4475ae6b84966024250c8b648
EFlatron <small.fin.murlock@gmail.com>
2021-07-20 15:22:40 +0300

    Create Main class

--- unit11reflection/src/main/java/Main.java
+++ unit11reflection/src/main/java/Main.java
@@ -1,5 +1,15 @@
+import properties.AppProperties;
+import properties.PropertiesConfig;
+import properties.PropertiesUtil;
+
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
+        Properties properties = PropertiesUtil.readProperties("unit11reflection/src/main/resources/app.properties");
 
+        PropertiesConfig propertiesConfig = new PropertiesConfig();
+        AppProperties appProperties = propertiesConfig.initialise(AppProperties.class, properties);
+        System.out.println(appProperties);
     }
 }
