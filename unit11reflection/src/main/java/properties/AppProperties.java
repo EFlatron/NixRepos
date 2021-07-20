b1d2d50e209bf0c5b8d64d721b4fcd4e78ce1a86
EFlatron <small.fin.murlock@gmail.com>
2021-07-20 14:01:40 +0300

    Create AppProperties and app.properties

--- unit11reflection/src/main/java/properties/AppProperties.java
+++ unit11reflection/src/main/java/properties/AppProperties.java
@@ -1,4 +1,34 @@
 package properties;
 
+import annotation.PropertyKey;
+
 public class AppProperties {
+    @PropertyKey(value = "person.firstName")
+    public String firstName;
+    @PropertyKey(value = "person.lastName")
+    public String lastName;
+    @PropertyKey(value = "person.age")
+    public int age;
+    @PropertyKey(value = "person.gender")
+    public Gender gender;
+    @PropertyKey(value = "person.yearOfBirth")
+    public int yearOfBirth;
+    @PropertyKey(value = "isPerson")
+    public boolean isPerson;
+
+    public enum Gender{
+        MALE, FEMALE;
+    }
+
+    @Override
+    public String toString() {
+        return "AppProperties{ " +
+                "FirstName='" + firstName + '\'' +
+                ", LastName='" + lastName + '\'' +
+                ", Age=" + age +
+                ", Gender=" + gender +
+                ", YearOfBirth=" + yearOfBirth +
+                ", IsPerson=" + isPerson +
+                '}';
+    }
 }
